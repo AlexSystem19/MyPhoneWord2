7fbf1ba5327ffb87904e9c697e258863e8132afe
Hugo Gutierrez <analistafuncional@SERVOSA.PE>
2024-08-19 22:45:57 -0500

    Agregar archivos de proyecto.

---
+++ MyPhoneWord2/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MyPhoneWord2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTraducir_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btnllamar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ MyPhoneWord2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MyPhoneWord2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
